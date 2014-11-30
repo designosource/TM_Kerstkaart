0da47d1189c1fd1cc761098a7bb4cca80b713ea9
Kristof Van Espen <kristof@creativebrain.be>
2014-11-30 14:28:08 +0100

    Added placeholder text and copyright

--- stap4.php
+++ stap4.php
@@ -24,8 +24,12 @@
 
 			<div id="back" style="display:none;">
 				<div id="backCon">
+					<div id="backSec">
 					<!-- add personal text here -->
-					<h1>Merry shitmas</h1>
+						<h1>Dag <span>John</span></h1>
+						<p>A wonderful serenity has taken possession of my entire soul, like these sweet mornings of spring which I enjoy with my whole heart. I am alone, and feel the charm of existence in this spot, which was created for the bliss of souls like mine. I am so happy, my dear friend, so absorbed in the exquisite sense of mere tranquil existence, that I neglect my talents. I should be incapable of drawing a single stroke at the present moment; and yet I feel that I never was a greater artist than now. When,.</p>
+						<span id="copy">© 2014 <a title="Website van Designosource" href="http://designosource.be/">designosource</a> - <a title="Website van IMD" href="http://weareimd.be/">Interactieve Multimedia Design</a></span>
+					</div>
 				</div>
 			</div>
 
