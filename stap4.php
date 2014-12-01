f7298949e0c9f02d50f1db9867d5fd2cceb54cc9
Kristof Van Espen <kristof@creativebrain.be>
2014-12-01 02:51:18 +0100

    Added dynamic card content

--- stap4.php
+++ stap4.php
@@ -32,13 +32,13 @@
 
 		<div id="container">
 			<div class="flipbox-container box100">
-			    <div id="cardCon" style="background-image: url('img/placeholderImg_1.png')">
+			    <div id="cardCon" style="background-image: url('<?php if(!empty($_SESSION['cardURL'])){echo $_SESSION['cardURL'];} ?>')">
 			    	<!-- Add poem here -->
 			    </div>
 		   	</div>
 
 			<div id="front" style="display:none;">
-				<div id="cardCon" style="background-image: url('img/placeholderImg_1.png')">
+				<div id="cardCon" style="background-image: url('<?php if(!empty($_SESSION['cardURL'])){echo $_SESSION['cardURL'];} ?>')">
 			    	<!-- Add poem here -->
 			    </div>
 			</div>
@@ -48,7 +48,7 @@
 					<div id="backSec">
 					<!-- add personal text here -->
 						<h1>Dag <span>John</span></h1>
-						<p>A wonderful serenity has taken possession of my entire soul, like these sweet mornings of spring which I enjoy with my whole heart. I am alone, and feel the charm of existence in this spot, which was created for the bliss of souls like mine. I am so happy, my dear friend, so absorbed in the exquisite sense of mere tranquil existence, that I neglect my talents. I should be incapable of drawing a single stroke at the present moment; and yet I feel that I never was a greater artist than now. When,.</p>
+						<p><?php if(!empty($_SESSION['persMess'])){ echo $_SESSION['persMess'];} ?></p>
 						
 					</div>
 
