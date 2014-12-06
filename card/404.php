35382824e50abd40a9df7874aa4e6119fb3ad91b
LiesbethVanaerschot <lies.vanaerschot@gmail.com>
2014-12-07 00:29:47 +0100

    404 page

--- card/404.php
+++ card/404.php
@@ -2,12 +2,23 @@
 <html lang="en">
 	<head>
 		<meta charset="UTF-8">
+
 		<link href="css/normalize.css" rel="stylesheet" type="text/css" />
-		<link href="css/style.css" rel="stylesheet" type="text/css" />
+		<link href="css/style.css" rel="stylesheet" type="text/css" media="all"/>
+
 		<title>Thomas More | Kerstkaart</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
 	</head>
 
 	<body>
-		<h1>Oops, looks like you're lost!</h1>
+		
+		<div id="frozen">
+			<img src="img/404-01.svg" alt="">
+			<h1>Hier is het ijskoud!</h1>
+			<h1>Ga maar snel naar een warmere plaats:</h1>
+				
+				<a href="http:\\ecard.thomasmore.be">De uitweg</a>
+		</div>
+
 	</body>
 </html>
