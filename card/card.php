e6d98366271d538d93825523df9c31109493c7dc
Kristof Van Espen <kristof@creativebrain.be>
2014-12-08 21:59:34 +0100

    Added head information

--- card/card.php
+++ card/card.php
@@ -46,8 +46,40 @@
 <html lang="en">
 	<head>
 		<meta charset="UTF-8">
+
+		<meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0;">
+		<link href="img/favicon.ico" rel="icon" type="image/x-icon" />
+
+		<meta name="description" content="Stuur een online kerstkaart naar je vrienden, familie, (mede)docenten en (mede)studenten als je werkgever, werknemer of student aan het Thomas More bent."/>
+		<meta name="keywords" content="Thomas more, Mechelen, Geel, Antwerpen, kerstkaart, kerstmis, ">
+		<meta name="author" content="Thomas More">
+
+		<meta property="og:title" content="Thomas More - Kerstkaart" />
+		<meta property="og:site_name" content="Thomas More - Kerstkaart" />
+		<meta property="og:url" content="http://ecard.thomasmore.be" />
+		<meta property="og:description" content="Stuur een online kerstkaart naar je vrienden, familie, (mede)docenten en (mede)studenten als je werkgever, werknemer of student aan het Thomas More bent."/>
+		<meta property="og:image" content="http://ecard.thomasmore.be/img/tm_kerstkaart_facebook.png"/>
+		<meta property="og:type" content="profile" /> 
+		<meta property="og:locale" content="en_US" />
+		<meta property="og:image:type" content="image/png" />
+		<meta property="og:image:width" content="1200" />
+		<meta property="og:image:height" content="630" />
+
+		<meta name="twitter:card" content="summary_large_image" />
+		<meta name="twitter:site" content="@ThomasMoreBE" />
+		<meta name="twitter:site:id" content="957208351" />
+		<meta name="twitter:creator" content="@ThomasMoreBE" />
+		<meta name="twitter:creator:id" content="957208351" />
+		<meta name="twitter:title" content="Thomas More - Kerstkaart" />
+		<meta name="twitter:description" content="Stuur een online kerstkaart naar je vrienden, familie, (mede)docenten en (mede)studenten als je werkgever, werknemer of student aan het Thomas More bent."/>
+		<meta name="twitter:image" content="http://ecard.thomasmore.be/img/tm_kerstkaart_twitter.png" />
+		<meta name="twitter:url" content="http://ecard.thomasmore.be" />
+		<meta name="twitter:domain" content="ttp://ecard.thomasmore.be">
+
 		<link href="css/normalize.css" rel="stylesheet" type="text/css" />
 		<link href="css/style.css" rel="stylesheet" type="text/css" />
+
 		<title>Thomas More | Kerstkaart</title>
 	</head>
 
